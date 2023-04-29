e3cba3b0ce2331536645107d9ed8954c222b7611
Kyeoeol <safari-earshot0a@icloud.com>
2023-04-29 19:56:46 +0900

    ✨ [create]: Create Project

---
+++ OCRCreditCard/OCRCreditCard/App/AppMain.swift
@@ -0,0 +1,42 @@
+//
+//  AppMain.swift
+//  OCRCreditCard
+//
+//  Created by kyeoeol on 2023/04/29.
+//
+
+import UIKit
+import SwiftUI
+
+@main
+struct AppMain {
+    static func main() {
+        if #available(iOS 14.0, *) { OCRCreditCardApp.main() }
+        else { ApplicationMain.main() }
+    }
+}
+
+
+
+
+// SwiftUI
+@available(iOS 14.0, *)
+struct OCRCreditCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+// AppDelegate
+struct ApplicationMain {
+    static func main() {
+        UIApplicationMain(
+            CommandLine.argc,
+            CommandLine.unsafeArgv,
+            nil,
+            NSStringFromClass(AppDelegate.self)
+        )
+    }
+}
